fdae52f2245bcfbc702ca55d9cc518e9f32d8c43
Guilherme Donato <donatocruzg@gmail.com>
2024-08-08 16:02:07 -0300

    adicionado a cars view no url

--- cars/views.py
+++ cars/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+<<<<<<< HEAD
 
 # Create your views here.
+=======
+from django.http import HttpResponse
+
+# Create your views here.
+
+def cars_view(request):
+    return HttpResponse('<h1>Meus carros valeu sapo</h1>')
+>>>>>>> 8c09817 (adicionada a página cars na url)
