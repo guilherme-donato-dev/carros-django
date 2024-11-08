4f355eb78edcfad73a71ad7f94e4e229248316c9
Guilherme Donato <donatocruzg@gmail.com>
2024-11-08 10:05:46 -0300

    atualizando

--- cars/views.py
+++ cars/views.py
@@ -1,12 +1,9 @@
 from django.shortcuts import render
-<<<<<<< HEAD
 
 # Create your views here.
-=======
 from django.http import HttpResponse
 
 # Create your views here.
 
 def cars_view(request):
-    return HttpResponse('<h1>Meus carros valeu sapo</h1>')
->>>>>>> 8c09817 (adicionada a página cars na url)
+    return HttpResponse('<h1>Meus carros top demaaaais</h1>')
