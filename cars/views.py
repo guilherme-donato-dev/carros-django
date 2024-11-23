6bed5e330f9b26c3a32657ad2a8c4d171b2955d1
guilherme-donato-dev <donatocruzg@gmail.com>
2024-11-23 17:32:22 -0300

    alterando para class based view

--- cars/views.py
+++ cars/views.py
@@ -1,25 +1,30 @@
 from django.shortcuts import render, redirect
 from cars.models import Car
 from cars.forms import CarModelForm
+from django.views import View
 
 # Create your views here.
 
 
 # Create your views here.
 
-def cars_view(request):
-    cars = Car.objects.all().order_by('value') #eu chamo todos os carros do meu banco de dados, a variável cars é uma query set comtodosos carros
-    search = request.GET.get('search') #aqui eu guardo a busca dele na variável search
 
-    if search: #se o user mandou uma busca, ele vai filtrar, se nao, passa direto e mostra todos
-        cars = cars.filter(model__icontains = search).order_by('model')
+class CarsView(View):
+
+    def get(self, request):
+        cars = Car.objects.all().order_by('value') #eu chamo todos os carros do meu banco de dados, a variável cars é uma query set comtodosos carros
+        search = request.GET.get('search') #aqui eu guardo a busca dele na variável search
+
+        if search: #se o user mandou uma busca, ele vai filtrar, se nao, passa direto e mostra todos
+            cars = cars.filter(model__icontains = search).order_by('model')
 
 
-    return render(
+        return render(
         request, 
         'cars.html',
         {'cars' : cars}
         )
+
 
 def new_car_view(request):
     if request.method == 'POST':
