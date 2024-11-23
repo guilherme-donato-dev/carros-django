ae16dd7c164e64fc28292a607d5d250e38f5439f
guilherme-donato-dev <donatocruzg@gmail.com>
2024-11-23 12:30:29 -0300

    finalizado a autenticaçao

--- accounts/views.py
+++ accounts/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render, redirect
-from django.contrib.auth.forms import UserCreationForm #form nativo do django específico para a criação do usuário
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm #form nativo do django específico para a criação do usuário
+from django.contrib.auth import authenticate, login, logout #método nativo do django
+
 
 # Create your views here.
 def register_view(request):
@@ -7,8 +9,29 @@ def register_view(request):
         user_form = UserCreationForm(request.POST)
         if user_form.is_valid():
             user_form.save()
-            return redirect('cars_list')
+            return redirect('login')
     else:
         user_form = UserCreationForm()
     return render(request, 'register.html',{'user_form' : user_form})
+
+
+def login_view(request):
+    if request.method == 'POST':
+        username = request.POST["username"]
+        password = request.POST["password"]
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            return redirect('cars_list')
+        else:
+            login_form = AuthenticationForm()
+
+
+    login_form = AuthenticationForm
+    return render(request, 'login.html', {'login_form' : login_form})
+
+
+def logout_view(request):
+    logout(request)
+    return redirect('cars_list')
     
