4e97d94608c9c3280fb351933278d9b1bac70721
Guilherme Donato <donatocruzg@gmail.com>
2024-11-28 18:08:57 -0300

    adicionado a listview e a createview

--- cars/views.py
+++ cars/views.py
@@ -1,38 +1,32 @@
+from typing import Any
+from django.db.models.query import QuerySet
 from django.shortcuts import render, redirect
 from cars.models import Car
 from cars.forms import CarModelForm
 from django.views import View
+from django.views.generic import ListView, CreateView #todos nativos do django
 
 # Create your views here.
 
 
 # Create your views here.
 
+    
+class CarsListView(ListView):  #class based view
+    model = Car #aqui eu aponto pro django qual o model que ele deve usar
+    template_name = 'cars.html' #aqui estou falando qual o template que ele deve usar
+    context_object_name = 'cars' 
 
-class CarsView(View):
+    def get_queryset(self):  #a ListView tem um método padrão para filtrar/procurar que é o metodo get queryset
+        cars = super().get_queryset().order_by('model')
+        search = self.request.GET.get('search')
+        if search:
+            cars = cars.filter(model__icontains = search)
+        return cars
+    
 
-    def get(self, request):
-        cars = Car.objects.all().order_by('value') #eu chamo todos os carros do meu banco de dados, a variável cars é uma query set comtodosos carros
-        search = request.GET.get('search') #aqui eu guardo a busca dele na variável search
-
-        if search: #se o user mandou uma busca, ele vai filtrar, se nao, passa direto e mostra todos
-            cars = cars.filter(model__icontains = search).order_by('model')
-
-
-        return render(
-        request, 
-        'cars.html',
-        {'cars' : cars}
-        )
-
-
-def new_car_view(request):
-    if request.method == 'POST':
-        new_car_form = CarModelForm(request.POST, request.FILES) #se o método for post, ele vai enviar pro banco de dados, se for GET,que é o else, ele so carrega a paginaem branco
-        if new_car_form.is_valid(): #fazendo a validação dos dados. is_valid é para checar a validação se estão corretas, as validações estão no forms.py
-            new_car_form.save()
-            return redirect('cars_list')  
-    else:
-        new_car_form = CarModelForm()
-    return render(request, 'new_car.html', {'new_car_form' : new_car_form }
-    )
+class NewCarCreateView(CreateView):  #como é CreateView, o django automaticamente já sabe que vai chegar get e post, e ele reconhece isso, então eu nao preciso passar a função de quando for get e quando for post. No GET ele vai renderizar o new_car.html e no post ele vai enviar o novo carro para o BD.
+    model = Car
+    form_class = CarModelForm #literalmente qual estilo de form você quer usar
+    template_name = 'new_car.html' #qual template que utilizo
+    success_url = '/cars/' #aqui ele pede qual url eu devo enviar o carro depois de cadastrar, que nesse caso é cars_list
